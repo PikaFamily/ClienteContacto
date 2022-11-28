f024f6333e3b012b5e29c0862294d45faa2b3da7
David Parejo Gonzalez <david2pg3@gmail.com>
2022-11-28 20:40:40 +0100

    First commit

---
+++ src/main/java/com/potecialcustomers/PC/PotencialCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.potecialcustomers.PC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotencialCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotencialCustomersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potecialcustomers/PC/PotencialCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potecialcustomers.PC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotencialCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
